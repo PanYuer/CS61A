0bb8218014e4fcbb582c6ec8a7b9f4024f220ca3
Yolanda Pan <panyolanda880@gmail.com>
2021-07-03 01:16:46 -0700

    last lab

---
+++ lab14/lab14.scm
@@ -0,0 +1,33 @@
+; Lab 14: Final Review
+
+(define (compose-all funcs)
+  'YOUR-CODE-HERE
+    (define (helper-composer funcs result)
+        (if (null? funcs)
+            result
+            (helper-composer (cdr funcs) ((car funcs) result))
+        )
+   )
+(lambda (x) (helper-composer funcs x))
+)
+
+
+(define (has-cycle? s)
+  (define (pair-tracker seen-so-far curr)
+    (cond ((null? curr) #f)
+          ((contains? seen-so-far curr) #t)
+          (else (pair-tracker (cons curr seen-so-far) (cdr-stream curr)))
+    )
+  )
+  (pair-tracker nil s)
+)
+
+(define (contains? lst s)
+  'YOUR-CODE-HERE
+  (cond
+    ((null? lst) #f)
+    ((eq? (car lst) s) #t)
+    (else (contains? (cdr lst) s))
+  )
+)
+
